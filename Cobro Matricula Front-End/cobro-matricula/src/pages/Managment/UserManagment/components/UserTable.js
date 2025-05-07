72e680a9123bd64c3a582e86390d1ae5569d1c0f
Jefferson Cargua <jeffersoncargua@gmail.com>
2025-05-07 12:04:24 -0500

    Se agregaron los componentes para realizar la gestion de usuarios

---
+++ Cobro Matricula Front-End/cobro-matricula/src/pages/Managment/UserManagment/components/UserTable.js
@@ -0,0 +1,64 @@
+
+export const UserTable = ({handleUpdatedUser,handleDeletedUser}) => {
+
+
+
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg"  >
+        <table className="w-full text-sm text-left rtl:text-right text-slate-900 dark:text-gray-400 mt-14">
+            <thead className="text-xs uppercase bg-gray-50/40 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" className="px-6 py-3">
+                        Nombre
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Apellido
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Telefono
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Correo
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        <span className="sr-only">Acciones</span>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr className="bg-white/80 border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50/90 dark:hover:bg-gray-600">
+                    <th scope="row" className="px-6 py-4 font-medium text-slate-900 whitespace-nowrap dark:text-white">
+                        Apple MacBook Pro 17"
+                    </th>
+                    <td className="px-6 py-4">
+                        Silver
+                    </td>
+                    <td className="px-6 py-4">
+                        Laptop
+                    </td>
+                    <td className="px-6 py-4">
+                        $2999
+                    </td>
+                    <td className="px-6 py-4 text-right flex justify-center gap-x-4 ">
+                        <button type="button" onClick={() => handleUpdatedUser()} className="px-2.5 py-2.5 bg-blue-500 hover:bg-blue-600 font-medium text-white dark:text-blue-500 flex items-center justify-center rounded-lg ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-person-fill-up me-2.5 w-5 h-5 " viewBox="0 0 16 16">
+                                <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.354-5.854 1.5 1.5a.5.5 0 0 1-.708.708L13 11.707V14.5a.5.5 0 0 1-1 0v-2.793l-.646.647a.5.5 0 0 1-.708-.708l1.5-1.5a.5.5 0 0 1 .708 0M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                                <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
+                            </svg>
+                            Editar
+                        </button>
+                        <button type="button" onClick={() => handleDeletedUser()} className="px-2.5 py-2.5 bg-red-500 hover:bg-red-600 font-medium text-white dark:text-blue-500 flex items-center justify-center rounded-lg ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-fill-x me-2.5 w-5 h-5" viewBox="0 0 16 16">
+                                <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
+                                <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m-.646-4.854.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 0 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 .708-.708"/>
+                            </svg>
+                            Eliminar
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+  )
+}
--- Cobro Matricula Front-End/cobro-matricula/src/pages/Managment/UserRegistration/components/FormRegistration.js
+++ Cobro Matricula Front-End/cobro-matricula/src/pages/Managment/UserRegistration/components/FormRegistration.js
@@ -21,47 +21,45 @@ export const FormRegistration = () => {
 
 
   return (
-    
-    <form className="group text-slate-50" onSubmit={() => handleSubmit()}>
+    <form className="group text-slate-900 p-4 border border-slate-100 rounded-lg bg-gradient-to-r from-indigo-500/75 from-10% via-sky-500 via-30% to-emerald-500/75 to-90%" onSubmit={() => handleSubmit()}>
         <div className="grid gap-6 mb-6 md:grid-cols-2  ">
             <div>
                 <label htmlFor="first_name" className="block mb-2 text-sm font-medium  dark:text-white">Nombre</label>
-                <input type="text" id="first_name" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+                <input type="text" id="first_name" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Roberta" required ref={nameRef} />
             </div>
             <div>
                 <label htmlFor="last_name" className="block mb-2 text-sm font-medium  dark:text-white">Apellido</label>
-                <input type="text" id="last_name" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
+                <input type="text" id="last_name" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Mendez" required ref={lastNameRef} />
             </div>
             <div>
                 <label htmlFor="city" className="block mb-2 text-sm font-medium  dark:text-white">Ciudad</label>
-                <input type="text" id="city" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Flowbite" required />
+                <input type="text" id="city" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Guayaquil" required ref={cityRef} />
             </div>  
             <div>
                 <label htmlFor="phone" className="block mb-2 text-sm font-medium  dark:text-white">Telefono: +593</label>
-                <input type="tel" id="phone" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
+                <input type="tel" id="phone" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0987654321" pattern="[0-9]{10}" required ref={phoneRef} />
             </div>
         </div>
         <div className="mb-6">
             <label htmlFor="email" className="block mb-2 text-sm font-medium  dark:text-white">Correo Electrónico</label>
-            <input type="email" id="email" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@city.com" required />
+            <input type="email" id="email" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example@example.com" required ref={emailRef} />
         </div> 
         <div className="mb-6">
             <label htmlFor="password" className="block mb-2 text-sm font-medium  dark:text-white">Contraseña</label>
-            <input type="password" id="password" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
+            <input type="password" id="password" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required ref={passRef} />
         </div> 
         <div className="mb-6">
             <label htmlFor="confirm_password" className="block mb-2 text-sm font-medium  dark:text-white">Confirmar Contraseña</label>
-            <input type="password" id="confirm_password" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
+            <input type="password" id="confirm_password" className="bg-gray-50 border border-gray-300  text-slate-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required ref={confirmPassRef} />
         </div> 
         <div className="flex items-start mb-6">
             <div className="flex items-center h-5">
             <input id="remember" type="checkbox" value="" onChange={() => setEnableRegistrationButton(!enableRegistrationButton)} className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
             </div>
-            <label htmlFor="remember" className="ms-2 text-sm font-medium  dark:text-gray-300">I agree with the <Link to='/' className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</Link>.</label>
+            <label htmlFor="remember" className="ms-2 text-sm font-medium  dark:text-gray-300">I agree with the <Link to='/' className="text-pink-600 hover:underline dark:text-blue-500">terms and conditions</Link>.</label>
         </div>
         
         <button type="submit" disabled={enableRegistrationButton ? false: true} className={`text-white ${enableRegistrationButton ? 'bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 cursor-pointer':'bg-blue-400 cursor-not-allowed'} font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>Registrar</button>
     </form>
-
   )
 }
